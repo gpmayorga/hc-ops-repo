41f27a6f8655b15ddb020970a06f208dbde8a472
Guillermo P <g.perez@liberit.net>
2016-11-13 23:06:17 -0500

    Added nexcloud role. Fix deploy_users role. Added inventory for nexcloud server

---
+++ terraform/mascloud.tf
@@ -0,0 +1,12 @@
+# Configure the OpenStack Provider
+provider "openstack" {
+    user_name  = "admin"
+    tenant_name = "admin"
+    password  = "pwd"
+    auth_url  = "http://myauthurl:5000/v2.0"
+}
+
+# Create a web server
+resource "openstack_compute_instance_v2" "test-server" {
+    ...
+}
